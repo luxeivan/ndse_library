244db895d38049904fe985b1d547083c8b337835
luxeivan <luxeivan@gmail.com>
2023-02-01 17:48:04 +0300

    commit1748

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,6 @@ COPY ./package*.json ./
 RUN npm install
 COPY ./public/* ./public
 COPY ./src ./src
+COPY ./model ./model
 
 CMD ["node","src/index.js"]
--- src/routes/books.js
+++ src/routes/books.js
@@ -1,60 +1,55 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const router = express.Router()
-const storeBook = require('../store/books')
+// const storeBook = require('../store/books')
 const fileMiddleware = require('../middleware/file');
-const serverCounter = process.env.SERVERCOUNTER || 'http://localhost:3001'
-const axios = require('axios')
+const serverMongo = process.env.SERVERMONGO || 'mongodb://root:example@mongo:27017/'
+const axios = require('axios');
+const Book = require('../../model/book');
+
+mongoose.connect(serverMongo)
 
 
-router.get('/', (req, res) => {
-    // res.json(storeBook.books)
-    // console.log(storeBook.books)
-    res.render('books/index', { books: storeBook.books, title: "Библиотека" })
-})
-router.get('/:id/download', (req, res) => {
-    const { id } = req.params
-    const index = storeBook.books.findIndex(item => item.id == id)
-    console.log(storeBook.books[index].fileBook)
-    // res.redirect(storeBook.books[index].fileBook);
-    res.download(storeBook.books[index].fileBook)
-})
-router.get('/book/:id', (req, res) => {
-    const { id } = req.params
-    const index = storeBook.books.findIndex(item => item.id === id)
-    if (index !== -1) {
-        //res.json(storeBook.books[index])
-        let count = 0
-        axios.get(serverCounter + `/counter/${id}`)
-            .then(function (response) {
-                // handle success
-                console.log(response.data);
-                if (response.data) {
-                    count = response.data
-                }
-                res.render('books/view', { book: storeBook.books[index], title: storeBook.books[index].title, count: count })
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-        axios.post(serverCounter + `/counter/${id}/incr`)
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-    } else {
-        res.status(404)
-        // res.json({ errcode: 404, errmsg: "not found book" })
-        res.render('404', { title: "Книга не найдена" })
+router.get('/', async (req, res) => {
+    try {
+        const books = await Book.find()
+        res.render('books/index', { books, title: "Библиотека" })
+    } catch (error) {
+        console.error(error)
     }
 })
-router.get('/create', (req, res) => {
-    // res.json(storeBook.books)
+router.get('/:id/download', async (req, res) => {
+    const { id } = req.params
+    try {
+        const book = await Book.findById(id)
+        console.log(book)
+        console.log('-------------------')
+        console.log(book.fileName)
+        res.download(book.fileName)
+
+    } catch (error) {
+        console.error(error)
+    }
+})
+router.get('/book/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const book = await Book.findById(id)
+        if (book) {
+            res.render('books/view', { book, title: book.title })
+        } else {
+            res.status(404)
+            res.render('404', { title: "Книга не найдена" })
+        }
+    } catch (error) {
+        console.error(error)
+    }
+})
+router.get('/create', async (req, res) => {
     res.render('books/create', { title: "Создать книгу" })
 })
-router.post('/create', fileMiddleware.single('fileBook'), (req, res) => {
+router.post('/create', fileMiddleware.single('fileBook'), async (req, res) => {
     let fileBook = null
-    //console.log(req)
     if (req.file) {
         fileBook = req.file.path;
     }
@@ -62,25 +57,29 @@ router.post('/create', fileMiddleware.single('fileBook'), (req, res) => {
         fileBook = fileBook.replaceAll('\\', '/')
     }
     const { title, description, authors, favorite, fileCover, fileName } = req.body
-    const newBook = new storeBook.classBook(title, description, authors, favorite, fileCover, fileName, fileBook)
-    storeBook.books.push(newBook)
+    const newBook = new Book({ title, description, authors, favorite, fileCover, fileName: fileBook })
+    try {
+        await newBook.save()
+    } catch (error) {
+        console.error(error)
+    }
     res.redirect('/books')
 })
-router.get('/update/:id', (req, res) => {
-    // res.json(storeBook.books)
+router.get('/update/:id', async (req, res) => {
     const { id } = req.params
-    const { title, description, authors, favorite, fileCover, fileName } = req.body
-    const index = storeBook.books.findIndex(item => item.id == id)
-    if (index !== -1) {
-        //res.json(storeBook.books[index])
-        res.render('books/update', { book: storeBook.books[index], title: "Редактировать книгу" })
-    } else {
-        res.status(404)
-        //res.json({ errcode: 404, errmsg: "not found book" })
-        res.render('404', { title: "Книга не найдена" })
+    try {
+        const book = await Book.findById(id)
+        if (book) {
+            res.render('books/update', { book, title: "Редактировать книгу" })
+        } else {
+            res.status(404)
+            res.render('404', { title: "Книга не найдена" })
+        }
+    } catch (error) {
+        console.error(error)
     }
 })
-router.post('/update/:id', fileMiddleware.single('fileBook'), (req, res) => {
+router.post('/update/:id', fileMiddleware.single('fileBook'), async (req, res) => {
     let fileBook = null
     if (req.file) {
         fileBook = req.file.path;
@@ -90,30 +89,41 @@ router.post('/update/:id', fileMiddleware.single('fileBook'), (req, res) => {
     }
     const { id } = req.params
     const { title, description, authors, favorite, fileCover, fileName } = req.body
-    const index = storeBook.books.findIndex(item => item.id == id)
-    if (index !== -1) {
-        storeBook.books[index] = {
-            ...storeBook.books[index], title, description, authors, favorite, fileCover, fileName, fileBook
+    try {
+        const book = await Book.findById(id)
+        if (book) {
+            try {
+                if (fileBook) {
+                    await Book.findByIdAndUpdate(id, { title, description, authors, favorite, fileCover, fileName: fileBook })
+                } else {
+                    await Book.findByIdAndUpdate(id, { title, description, authors, favorite, fileCover})
+
+                }
+                res.redirect(`/books`)
+            } catch (error) {
+                console.error(error)
+            }
+        } else {
+            res.status(404)
+            res.render('404', { title: "Книга не найдена" })
         }
-        // res.json(storeBook.books[index])
-        res.redirect(`/books`)
-    } else {
-        res.status(404)
-        //res.json({ errcode: 404, errmsg: "not found book" })
-        res.render('404', { title: "Книга не найдена" })
+    } catch (error) {
+        console.error(error)
     }
 })
-router.post('/delete/:id', (req, res) => {
+router.post('/delete/:id', async (req, res) => {
     const { id } = req.params
-    const index = storeBook.books.findIndex(item => item.id == id)
-    if (index !== -1) {
-        storeBook.books.splice(index, 1)
-        // res.json("ok")
-        res.redirect('/books')
-    } else {
-        res.status(404)
-        // res.json({ errcode: 404, errmsg: "not found book" })
-        res.render('404', { title: "Книга не найдена" })
+    try {
+        const book = await Book.findById(id)
+        if (book) {
+            await Book.deleteOne({ _id: id })
+            res.redirect('/books')
+        } else {
+            res.status(404)
+            res.render('404', { title: "Книга не найдена" })
+        }
+    } catch (error) {
+        console.error(error)
     }
 })
 
